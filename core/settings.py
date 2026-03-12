94d79e8dd8080c1716d6e0d8b900f081f6cb91d9
bernardondff <bernardondf@gmail.com>
2026-03-12 18:34:02 -0300

    remove apps usarios e servicos, cria estrutura apps/accounts

--- core/settings.py
+++ core/settings.py
@@ -41,8 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios',
-    'servicos',
 ]
 
 MIDDLEWARE = [
@@ -126,7 +124,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-
-# Model de usuário customizado
-AUTH_USER_MODEL = 'usuarios.User'
