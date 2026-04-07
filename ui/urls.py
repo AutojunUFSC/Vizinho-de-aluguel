6d8c5f13567d16a0aa8c88279cec5685d8df3729
Gabriel Madeira <gabrielmadeiracamargo@gmail.com>
2026-04-06 21:59:22 -0300

    acho que esqueci de escrever o commit

--- ui/urls.py
+++ ui/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home),
+    path('cadastro/', views.cadastro, name='cadastro')
 ]
--- ui/views.py
+++ ui/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "ui/home.html")
+
+def cadastro(request):
+    return render(request, "ui/cadastro.html")
