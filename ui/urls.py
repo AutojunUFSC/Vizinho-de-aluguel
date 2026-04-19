315687f608b00b65dea64088ebf84207cfa4167f
Gabriel Madeira <gabrielmadeiracamargo@gmail.com>
2026-04-19 16:21:13 -0300

    Novas páginas e hiperlinks

--- ui/urls.py
+++ ui/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("", views.home),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('usuario/', views.home_usuario, name='home_usuario'),
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('nova_solicitacao/', views.nova_solicitacao, name='nova_solicitacao'),
+    path('minhas_solicitacoes/', views.minhas_solicitacoes, name='minhas_solicitacoes')
 ]
