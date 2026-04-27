633fb39d91a2da91f056c54bbd105236d32f74d5
bernardondff <bernardondf@gmail.com>
2026-04-27 19:20:06 -0300

    integração da minhas solicitacoes

---
+++ static/js/auth.js
@@ -0,0 +1,60 @@
+const TOKEN_KEY    = 'vda_token';
+const USER_TYPE_KEY = 'vda_user_type';
+
+function getAuthHeaders() {
+    const token = localStorage.getItem(TOKEN_KEY);
+    if (!token) return {};
+    return { 'Authorization': 'Bearer ' + token };
+}
+
+function logout() {
+    localStorage.removeItem(TOKEN_KEY);
+    localStorage.removeItem(USER_TYPE_KEY);
+    window.location.href = '/login/';
+}
+
+function requireAuth() {
+    if (!localStorage.getItem(TOKEN_KEY)) {
+        window.location.href = '/login/';
+    }
+}
+
+function redirectIfLogged() {
+    const token = localStorage.getItem(TOKEN_KEY);
+    if (!token) return;
+    redirectByUserType(localStorage.getItem(USER_TYPE_KEY));
+}
+
+function redirectByUserType(userType) {
+    if (userType === 'CIDADAO') {
+        window.location.href = '/home-usuario/';
+    } else if (userType === 'MEI') {
+        window.location.href = '/feed/';
+    } else {
+        window.location.href = '/admin/';
+    }
+}
+
+function escHtml(str) {
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
+function parseApiErrors(data) {
+    if (typeof data === 'string') return data;
+    const messages = [];
+    for (const key of Object.keys(data)) {
+        const val   = data[key];
+        const label = (key === 'non_field_errors' || key === 'detail') ? '' : key + ': ';
+        if (Array.isArray(val)) {
+            messages.push(label + val.join(' '));
+        } else {
+            messages.push(label + val);
+        }
+    }
+    return messages.join('\n');
+}
