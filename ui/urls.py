cced3d290acf4c7eb5445ef1ca7e5e5da383ffcf
Rafael Mussi <rafabmussi@gmail.com>
2026-04-30 13:10:45 -0300

    Adicionada a pagina contato, alto contraste e mapa do site

--- ui/urls.py
+++ ui/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('solicitacao/<str:pk>/propostas/', views.propostas_solicitacao, name='propostas_solicitacao'),
     path('solicitacao/<str:pk>/proposta/', views.fazer_proposta, name='fazer_proposta'),
     path('pedido/<str:pk>/', views.acompanhamento_pedido, name='acompanhamento_pedido'),
+    path('mapa-do-site/', views.sitemap, name='sitemap'),
 ]
