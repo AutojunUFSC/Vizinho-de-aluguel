bf1af25df99580c55a2cd85465434cef518d06a8
Rafael Mussi <rafabmussi@gmail.com>
2026-05-11 10:24:27 -0300

    chore: atualiza settings com CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
-
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000', cast=Csv())
 
 # Application definition
 
